22f28f11def8b2df63234c176e55c9206756dcb5
John Hart <john.hart@yahoo.com>
2016-08-26 15:27:04 -0400

    Moved the exported functions into a class and revised the overall project structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,117 +1,99 @@
+'use strict';
+
 var gulp = require('gulp');
 var runSequence = require('run-sequence');
-var del = require('del');
+
+var node = './node_modules';
+
+var src = './src';
+var debug = './debug';
+var release = './release';
+
+function clean(patterns) {
+    console.log('deleting: ' + patterns);
+    console.log('Note: should this fail you might have directories open in the terminal');
+
+    var del = require('del');
+
+    return del(patterns);
+}
 
 gulp.task('delete-node-modules', function () {
-    var patterns = ['./node_modules/'];
-
-    console.log('deleting: ' + patterns);
-    console.log('Note: should this fail you might have one of the directories open in the terminal');
-
-    return del(patterns); // must return the del() result or intermittent and mysterious stream error may occur
+    return clean([node]);
 });
 
-gulp.task('CLEAN', function () {
-    var patterns = ['./app/', './map/', './distro/', './*.log'];
-
-    console.log('deleting: ' + patterns);
-    console.log('Note: should this fail you might have one of the directories open in the terminal');
-
-    return del(patterns); // must return the del() result or intermittent and mysterious stream error will occur
+gulp.task('clean', function () {
+    return clean([debug, release, './*.log']);
 });
 
-gulp.task('copy-images', function () {
-    return gulp.src('./src/**/*.+(ico|gif|jpg|png)')
-        .pipe(gulp.dest('./app/'));
-});
-
-gulp.task('compile-css', function () {
-    var ext_replace = require('gulp-ext-replace');
-    var postcss = require('gulp-postcss');
-    var autoprefixer = require('autoprefixer');
-    var precss = require('precss');
-    var cssnano = require('cssnano');
-    var sourcemaps = require('gulp-sourcemaps');
-
-    return gulp.src('./src/**/*.scss')
-        .pipe(sourcemaps.init())
-        .pipe(postcss([precss, autoprefixer, cssnano]))
-        .pipe(sourcemaps.write('../map/'))
-        .pipe(ext_replace('.css'))
-        .pipe(gulp.dest('./app/'));
-});
-
-gulp.task('copy-html', function () {
-    return gulp.src('./src/**/*.html')
-        .pipe(gulp.dest('./app/'));
-});
-
-gulp.task('copy-javascript', function () {
-    return gulp.src(['./src/**/*.js', '!./src/**/spec*.js'])
-        .pipe(gulp.dest('./app/'));
-});
-
-gulp.task('copy-test-javascript', function () {
-    return gulp.src('./src/**/spec*.js')
-        .pipe(gulp.dest('./app/'));
-});
-
-var map = '../map/';
-var app = './app/';
-
-gulp.task('compile-typescript', function () {
+gulp.task('compile-modules', function () {
     var sourcemaps = require('gulp-sourcemaps');
     var typescript = require('gulp-typescript');
     var merge = require('merge2');
 
     var src = gulp.src(['!./src/**/spec*.ts', './src/**/*.ts']);
-    var dst = gulp.dest(app);
+    var dst = gulp.dest(release);
 
-    var jsMap = src.pipe(typescript(typescript.createProject('tsconfig.json')))
-        .pipe(sourcemaps.init())
-        .pipe(sourcemaps.write(map));
-
+    var js = src.pipe(typescript(typescript.createProject('tsconfig.json')));
     var dts = src.pipe(typescript(typescript.createProject('tsconfig.json')));
 
     return merge([
-        jsMap.pipe(dst),   // writes .js and .map files
-        dts.dts.pipe(dst)  // writes .d.ts files
+        js.pipe(dst),     // writes .js files
+        dts.dts.pipe(dst) // writes .d.ts files
     ]);
 });
 
-gulp.task('compile-test-typescript', function () {
+gulp.task('compile-modules-with-maps', function () {
     var sourcemaps = require('gulp-sourcemaps');
     var typescript = require('gulp-typescript');
 
-    return gulp.src(['./src/**/spec*.ts'])
-        .pipe(typescript(typescript.createProject('tsconfig.json')))
-        .pipe(sourcemaps.init())
-        .pipe(sourcemaps.write(map))
-        .pipe(gulp.dest(app));
+    return gulp.src([src + '/**/*.ts', '!' + src + '/**/*spec.ts', './typings.d.ts'])
+               .pipe(typescript(typescript.createProject('./tsconfig.json')))
+               .pipe(sourcemaps.init())
+               .pipe(sourcemaps.write('./'))
+               .pipe(gulp.dest(debug + '/'));
 });
 
-gulp.task('build-distribution', function () {
-    return gulp.src(['./src/package.json', './README.md', './app/**/*.js', './app/**/*.d.ts', '!./app/**/*.shim.js', '!./node_modules/', '!./node_modules/**', '!./typings/', '!./typings/**'])
-        .pipe(gulp.dest('distro'));
+gulp.task('compile-tests', function () {
+    var sourcemaps = require('gulp-sourcemaps');
+    var typescript = require('gulp-typescript');
+
+    return gulp.src([src + '/**/spec*.ts', './typings.d.ts'])
+               .pipe(typescript(typescript.createProject('./tsconfig.json')))
+               .pipe(sourcemaps.init())
+               .pipe(sourcemaps.write('./'))
+               .pipe(gulp.dest(debug + '/'));
 });
 
-// Do not automatically perform a CLEAN when building the tests because Karma tends to gets stuck after files that it is monitoring are deleted
-
-gulp.task('BUILD-TESTS', function () {
-    runSequence('compile-typescript', ['copy-images', 'compile-css', 'copy-html', 'copy-javascript'], 'compile-test-typescript', 'copy-test-javascript');
+gulp.task('copy-static', function () {
+    return gulp.src(['./src/package.json', './README.md'])
+               .pipe(gulp.dest(release + '/'));
 });
 
-gulp.task('BUILD-CLEAN-APPLICATION', function () {
-    runSequence('CLEAN', 'compile-typescript', ['copy-images', 'compile-css', 'copy-html', 'copy-javascript']);
+gulp.task('build-release', function (callback) {
+    return runSequence(
+        'clean',
+        'compile-modules',
+        'copy-static',
+        callback);
 });
 
-gulp.task('BUILD-CLEAN-DISTRIBUTION', function () {
-    runSequence('CLEAN', 'compile-typescript', ['copy-images', 'compile-css', 'copy-html', 'copy-javascript'], 'build-distribution');
+gulp.task('build-debug', function (callback) {
+    return runSequence(
+        'clean',
+        'compile-modules-with-maps',
+        'compile-tests',
+        callback);
 });
 
-gulp.task('WATCH', ['CLEAN'], function () {
-    gulp.watch(['./src/**/*'], ['BUILD-TESTS']);
+// Do not automatically perform a clean files when rebuilding the tests because Karma in live mode tends to gets stuck when files that it is monitoring are deleted
+
+gulp.task('watch', function () {
+    gulp.watch([src + '/**/*.ts'], ['compile-modules-with-maps', 'compile-tests']);
 });
 
-gulp.task('default', ['WATCH'], function () {
+gulp.task('default', function (callback) {
+    return runSequence('build-debug',
+                       'watch',
+                       callback);
 });
--- src/spec.ts
+++ src/spec.ts
@@ -1,22 +1,17 @@
-// typings install jasmine --ambient
-
 ///<reference path="../typings/main/ambient/jasmine/jasmine.d.ts" />
-///<reference path="xdom.ts"/>
 
-import nodeTypeKeys = xdom.nodeTypeKeys;
-import nodeTypes = xdom.nodeTypes;
-import nodeTypeNames = xdom.nodeTypeNames;
-
-import nodeTypeKey = xdom.nodeTypeKey;
-import isNodeTypeKey = xdom.isNodeTypeKey;
-import nodeTypeName = xdom.nodeTypeName;
-import nodeType = xdom.nodeType;
+import {XDom, nodeTypes, nodeTypeNames, nodeTypeKeys, namespaceSeparator} from './xdom';
 
 describe('autopulous-xdom unit test suite', () => {
     beforeAll(() => {
+        console.log('namespaceSeparator: \"' + namespaceSeparator + '\"');
         console.log('nodeTypes:' + JSON.stringify(nodeTypes));
         console.log('nodeTypeKeys:' + nodeTypeKeys);
         console.log('nodeTypeNames:' + nodeTypeNames);
+    });
+
+    it('should validate the namespace delimiter character', () => {
+        expect(':').toEqual(namespaceSeparator);
     });
 
     it('should have properly sized constant arrays', () => {
@@ -27,7 +22,7 @@ describe('autopulous-xdom unit test suite', () => {
     it('should have monotonically ascending numeric member values', () => {
         expect(nodeTypes.UNDEFINED).toEqual(0);
 
-        for (var index:number = nodeTypes.ELEMENT; nodeTypes.NOTATION >= index; index++) {
+        for (var index: number = nodeTypes.ELEMENT; nodeTypes.NOTATION >= index; index++) {
             expect(+nodeTypeKeys[index]).toEqual(index);
         }
     });
@@ -35,43 +30,43 @@ describe('autopulous-xdom unit test suite', () => {
     it('should have monotonically ascending member values addressed by key names', () => {
         expect(nodeTypes[<any>nodeTypeKeys[nodeTypes.NOTATION + 1]]).toEqual(0);
 
-        for (var index:number = nodeTypes.ELEMENT; nodeTypes.NOTATION >= index; index++) {
+        for (var index: number = nodeTypes.ELEMENT; nodeTypes.NOTATION >= index; index++) {
             expect(nodeTypes[<any>nodeTypeKeys[index + nodeTypes.NOTATION + 1]]).toEqual(index);
         }
     });
 
     it('should pass numeric nodeKey verification', () => {
-        expect(false).toEqual(isNodeTypeKey('13'));
+        expect(false).toEqual(XDom.isNodeTypeKey('13'));
 
-        for (var index:number = nodeTypes.ELEMENT; nodeTypes.NOTATION >= index; index++) {
-            expect(true).toEqual(isNodeTypeKey(nodeTypeKeys[index]));
+        for (var index: number = nodeTypes.ELEMENT; nodeTypes.NOTATION >= index; index++) {
+            expect(true).toEqual(XDom.isNodeTypeKey(nodeTypeKeys[index]));
         }
     });
 
     it('should pass string nodeKey verification', () => {
-        expect(false).toEqual(isNodeTypeKey('BOGUS'));
+        expect(false).toEqual(XDom.isNodeTypeKey('BOGUS'));
 
-        for (var index:number = nodeTypes.ELEMENT; nodeTypes.NOTATION >= index; index++) {
-            expect(true).toEqual(isNodeTypeKey(nodeTypeKeys[index + nodeTypes.NOTATION]));
+        for (var index: number = nodeTypes.ELEMENT; nodeTypes.NOTATION >= index; index++) {
+            expect(true).toEqual(XDom.isNodeTypeKey(nodeTypeKeys[index + nodeTypes.NOTATION]));
         }
     });
 
     it('should be able to retrieve nodeTypeKey by index', () => {
-        for (var index:number = nodeTypes.ELEMENT; nodeTypes.NOTATION >= index; index++) {
-            expect(index).toEqual(+nodeTypeKey(index));
+        for (var index: number = nodeTypes.ELEMENT; nodeTypes.NOTATION >= index; index++) {
+            expect(index).toEqual(+XDom.nodeTypeKey(index));
         }
     });
 
     it('should be able to retrieve a nodeType using a nodeType (reflexive)', () => {
-        for (var index:number = nodeTypes.ELEMENT; nodeTypes.NOTATION >= index; index++) {
-            expect(nodeTypeKeys[index + nodeTypes.NOTATION + 1]).toEqual(nodeType(nodeTypeKey(index)));
+        for (var index: number = nodeTypes.ELEMENT; nodeTypes.NOTATION >= index; index++) {
+            expect(nodeTypeKeys[index + nodeTypes.NOTATION + 1]).toEqual(XDom.nodeType(XDom.nodeTypeKey(index)));
         }
     });
 
     it('should be able to retrieve a the text description of a nodeType', () => {
-        expect(undefined).toEqual(nodeTypeName(nodeTypes.UNDEFINED));
-        expect("Element").toEqual(nodeTypeName(nodeTypes.ELEMENT));
-        expect("Notation").toEqual(nodeTypeName(nodeTypes.NOTATION));
-        expect(undefined).toEqual(nodeTypeName(nodeTypes.NOTATION + 1));
+        expect(undefined).toEqual(XDom.nodeTypeName(nodeTypes.UNDEFINED));
+        expect("Element").toEqual(XDom.nodeTypeName(nodeTypes.ELEMENT));
+        expect("Notation").toEqual(XDom.nodeTypeName(nodeTypes.NOTATION));
+        expect(undefined).toEqual(XDom.nodeTypeName(nodeTypes.NOTATION + 1));
     });
 });
